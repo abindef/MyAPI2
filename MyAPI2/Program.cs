e82276bf915cb756ba96c8b79c0b7c8dd93bb786
abindef <abindef@hotmail.com>
2022-07-08 17:04:00 +0800

    添加项目文件。

---
+++ MyAPI2/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+//builder.WebHost.UseUrls("http://*:6001");
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
